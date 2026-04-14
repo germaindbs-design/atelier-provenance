8547b9cd3460b75fd37cf957c7e8dcb05a19897a
germaindbs-design <germain.dbs@gmail.com>
2026-04-14 14:56:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/mentions-legales',
+        destination: '/legal/mentions-legales',
+        permanent: true,
+      },
+      {
+        source: '/cgv',
+        destination: '/legal/cgv',
+        permanent: true,
+      },
+      {
+        source: '/confidentialite',
+        destination: '/legal/confidentialite',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
